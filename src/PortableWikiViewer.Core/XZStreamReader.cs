7328ba14b82834d9b51448dfdd2a817804a9fed4
Sam Bott <sam.bott@gmail.com>
2015-08-31 15:55:42 +0100

    Added class to check CRC

--- src/PortableWikiViewer.Core/XZStreamReader.cs
+++ src/PortableWikiViewer.Core/XZStreamReader.cs
@@ -1,12 +1,16 @@
 ﻿using System;
+using System.IO;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
 namespace PortableWikiViewer.Core
 {
-    public class Class1
+    public class XZStreamReader
     {
-        public static int Test = 1;
+        public XZStreamReader(Stream stream)
+        {
+
+        }
     }
 }
